775f5edd79970a212f01508ce22217b221c58605
venkatesh-21 <rajavenkatesh610@gmail.com>
2025-07-17 11:35:30 +0530

    final commit

--- src/dictionary.jsx
+++ src/dictionary.jsx
@@ -37,7 +37,7 @@ const Dictionary = () => {
         <input type="text" placeholder="Search for a Word" onChange={onChange} />
         <button onClick={onClick}>Search</button>
     </div>
-    <h3>Definition</h3>
+    <h3>Definition:</h3>
     <p>{submit}</p>
   </div>;
 };
